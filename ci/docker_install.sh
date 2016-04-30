09d767cdaaf64b3a048e3cb64a8b4b84cddd7b71
John Martin <john@groupm7.com>
2016-04-29 23:10:13 -0400

    Update docker_install.sh

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -9,10 +9,11 @@ set -xe
 apt-get update -yqq
 apt-get install git -yqq
 
-# Install phpunit, the tool that we will use for testing
-curl -o /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
-chmod +x /usr/local/bin/phpunit
-
 # Install mysql driver
 # Here you can install any other extension that you need
 docker-php-ext-install pdo_mysql
+
+php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+php -r "if (hash_file('SHA384', 'composer-setup.php') === 'a52be7b8724e47499b039d53415953cc3d5b459b9d9c0308301f867921c19efc623b81dfef8fc2be194a5cf56945d223') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
